4f4815c142028bb515e8963ad2e5c6d39fdd6888
adomlishvili <nextrobass573@gmail.com>
2024-05-12 22:49:28 +0400

    first commit

---
+++ day 1/day1/day1/lesson.py
@@ -0,0 +1,15 @@
+name= "gio"
+
+surname = "adomlishvili"
+
+age = 15
+knows_programming = True
+height = 178
+print(name)
+print(name + " " + surname)
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
+print(name + " " + str(age))
