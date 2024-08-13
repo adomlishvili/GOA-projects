f254621ab0f2033c13cd8965e21037e3abc5cf1d
adomlishvili <nextrobass573@gmail.com>
2024-08-13 18:34:07 +0400

    Add files via upload

---
+++ level 14/homework/claswork/claswork.py
@@ -0,0 +1,8 @@
+my_list = [1,2,3]
+my2_list = [3,4,5]
+my3_list = [6,7,8,9]
+my4_list = [10,11,12,13,14,]
+my5_list = [15,16,17,18,19]
+
+pos1 = [my_list[i]for i in range (len(my_list))]
+neg1 =[my_list[-i-1]for i in range (len(my_list))]
---
+++ level 14/homework/homework.py
@@ -0,0 +1,13 @@
+# my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150,]
+# for i in range (len(my_list)):
+#     for i in range (-len(my_list),0):
+#         print(f"my list[{i}]={my_list[i]}")
+
+
+
+
+gyo_wrldd = [10, 20, 30, 40, 50, 60, 70, 80, 90, 110, 120, 130, 140, 150,]
+
+first_three = gyo_wrldd[:3]
+second_three = gyo_wrldd[9:3]
+        
