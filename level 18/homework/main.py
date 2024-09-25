36a47173476d06f6f2d25321a33cd80412d02f01
adomlishvili <nextrobass573@gmail.com>
2024-09-25 23:37:43 +0400

    Add files via upload

---
+++ level 18/homework/main.py
@@ -0,0 +1,87 @@
+num1 = int(15)
+num2 = int(15)
+print(num1 + num2)
+
+string1 = "გამარჯობა"
+string2 = "მსოფლიო"
+result = string1 + " " + string2
+print(result)
+
+
+
+a = 5
+b = 2
+result = a / b
+print(result)
+
+
+
+a = 5
+b = 3
+
+print(a == b)   # False
+print(a != b)   # True
+print(a > b)    # True
+print(a < b)    # False
+print(a >= b)   # True
+print(a <= b)   # False
+
+
+
+print(5 + 5 == 8 + 2)  
+print(10 - 3 > 5 - 2)  
+print(3 * 4 <= 12)     
+print(16 / 4 != 3)     
+print((2 + 2) * 3 == 12 / 2)  
+
+
+
+
+
+
+
+print(True and True)    # True
+print(True and False)   # False
+print(False and True)   # False
+print(False and False)  # False
+
+print(True or True)     # True
+print(True or False)    # True
+print(False or True)    # True
+print(False or False)   # False
+
+print(not True)         # False
+print(not False)        # True
+
+
+
+
+
+
+
+for i in range(1, 11):
+    print(i)
+
+
+
+
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+total = 0
+
+for num in numbers:
+    total += num
+
+print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
