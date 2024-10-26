5c3f201ffcf713bb0ad31ed58335ee235132982e
adomlishvili <nextrobass573@gmail.com>
2024-10-26 23:34:09 +0400

    Add files via upload

---
+++ level 06/homework/level6/homework.py
@@ -0,0 +1,4 @@
+email = input("pleas enter email")
+name  = input("pleas enter name")
+number = int(input("pleas enter number"))
+password = input("pleas enter password")
---
+++ level 12/homework/homework3.py
@@ -0,0 +1,5 @@
+number = int(input("enter any number"))
+if number % 2 == 0:
+    print("its even")
+else:
+    print("its odd")
