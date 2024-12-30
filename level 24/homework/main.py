c65fd861b3b0002ff0ecc06d98b554610e10aa3a
adomlishvili <nextrobass573@gmail.com>
2024-12-30 20:59:07 +0400

    Add files via upload

---
+++ level 24/homework/main.py
@@ -0,0 +1,17 @@
+# a list of three elements
+ages = [19, 26, 29]
+print(ages)
+
+# Output: [19, 26, 29]
+
+
+
+
+
+
+
+# Access the first element
+print(languages[0])   # Python
+
+# Access the third element
+print(languages[2])   # C++
